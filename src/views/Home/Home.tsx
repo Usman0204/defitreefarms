e0c006ee8bba550cedb5f0a06a5352cd75dc9cbb
Usman Aslam Malik <usman.maliknu13@gmail.com>
2021-07-13 18:39:57 -0400

    changes

---
+++ src/views/Home/Home.tsx
@@ -0,0 +1,121 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Heading, Text, BaseLayout } from '@pancakeswap-libs/uikit'
+import useI18n from 'hooks/useI18n'
+import Page from 'components/layout/Page'
+import FarmStakingCard from 'views/Home/components/FarmStakingCard'
+import LotteryCard from 'views/Home/components/LotteryCard'
+import CakeStats from 'views/Home/components/CakeStats'
+import TotalValueLockedCard from 'views/Home/components/TotalValueLockedCard'
+import EarnAPYCard from 'views/Home/components/EarnAPYCard'
+import EarnAssetCard from 'views/Home/components/EarnAssetCard'
+import WinCard from 'views/Home/components/WinCard'
+import TreeStats from './components/TreeStats'
+
+const Hero = styled.div`
+  align-items: center;
+  background-image: url('/images/pan-bg-mobile.svg');
+  background-repeat: no-repeat;
+  background-position: top center;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  margin: auto;
+  margin-bottom: 32px;
+  padding-top: 116px;
+  text-align: center;
+
+  ${({ theme }) => theme.mediaQueries.lg} {
+    background-image: url('/images/merch-banner.png')
+    background-position: left center, right center;
+    height: 165px;
+    padding-top: 0;
+    width: 100%;
+    background-image: url(/images/merch-banner.png);
+    background-repeat: no-repeat;
+    background-position: center center;
+    min-height: 115px;
+    background-size: auto
+  }
+`
+
+const Cards = styled(BaseLayout)`
+  align-items: stretch;
+  justify-content: stretch;
+  margin-bottom: 32px;
+
+  & > div {
+    grid-column: span 6;
+    width: 100%;
+  }
+
+  ${({ theme }) => theme.mediaQueries.sm} {
+    & > div {
+      grid-column: span 8;
+    }
+  }
+
+  ${({ theme }) => theme.mediaQueries.lg} {
+    & > div {
+      grid-column: span 6;
+    }
+  }
+`
+
+const CTACards = styled(BaseLayout)`
+  align-items: start;
+  margin-bottom: 32px;
+
+  & > div {
+    grid-column: span 6;
+  }
+
+  ${({ theme }) => theme.mediaQueries.sm} {
+    & > div {
+      grid-column: span 8;
+    }
+  }
+
+  ${({ theme }) => theme.mediaQueries.lg} {
+    & > div {
+      grid-column: span 4;
+    }
+  }
+`
+
+const Home: React.FC = () => {
+  const TranslateString = useI18n()
+
+  return (
+    <Page>
+      <Hero>
+        <Heading as="h1" size="xl" mb="24px" color="secondary">
+          {TranslateString(576, 'Official Merchandise!')}
+        </Heading>
+        <Text>{TranslateString(578, 'Get our fantastreec merch for free!')}</Text>
+      </Hero>
+      <div>
+        <Cards>
+          <FarmStakingCard />
+          <LotteryCard />
+        </Cards>
+        
+        <Cards>
+          <CakeStats />
+          <TreeStats />
+         
+        </Cards>
+        <Cards>
+        <TotalValueLockedCard />
+        </Cards>
+        <CTACards>
+          <EarnAPYCard />
+          <EarnAssetCard />
+          <WinCard />
+        </CTACards>
+      </div>
+    </Page>
+  )
+}
+
+export default Home
